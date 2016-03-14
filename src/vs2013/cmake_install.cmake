d854850c1be8e08f69ca6609e2d46ff46273e0ba
fengbc <fengbingchun@sensetime.com>
2016-03-14 10:16:27 +0800

    Modify libcurl install path

--- src/vs2013/cmake_install.cmake
+++ src/vs2013/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/CURL")
+  set(CMAKE_INSTALL_PREFIX "E:/GitCode/Libcurl_Test/src")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
